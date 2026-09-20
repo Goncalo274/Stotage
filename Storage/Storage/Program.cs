c16e82a340ed4ba7882f2096249c78084d0f1923
Goncalo274 <1025060@alunos.ruizcosta.edu.pt>
2026-09-20 19:28:29 +0100

    Add files via upload

---
+++ Storage/Storage/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class Program
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; } = string.Empty;
+        public double price { get; set; }
+
+    }
+    public static void Menu()
+    {
+        bool op = false;
+        
+        while(op = true)
+        {
+            Console.WriteLine("=== Main Menu");
+            Console.WriteLine("1-> Manage product");
+            Console.WriteLine("0-> exit\n");
+            Console.Write("Choose: ");
+
+            if (int.TryParse(Console.ReadLine(), out int choose))
+            {
+                
+            }
+            else
+            {
+
+            }
+        }
+
+    }
+    static void Main()
+    {
+        Menu();
+
+        Product product = new Product();
+    }
+}
